d035fa17a993994aa6f8fd0dd363c620be05fe4c
Jonah Rubin <jonah@jonaherubin.com>
2017-12-16 16:28:51 -0700

    wrote example program and built abstract syntax grammer

--- DaystromSyntax.hs
+++ DaystromSyntax.hs
@@ -1,5 +1,38 @@
 module DaystromSyntax where
 
 type Name = String
+type Stringlit = String
+type Value = Integer
 
 data Program = DaystromProgram [Alias] [Function]
+        deriving (Show)
+
+data Alias = StationAlias Name Name
+        deriving (Show)
+
+data Function = HelperFunction Name [Command] | MainFunction [Command]
+        deriving (Show)
+
+data Command = Com Name Instruction
+        deriving (Show)
+
+data Instruction = PushNum Value
+                 | PushVar Name
+                 | PopVar  Name
+                 | StartOutputStandard
+                 | StartOutputError
+                 | PrintString String
+                 | PrintVal
+                 | EndOutput
+                 | GetValue
+                 | Swap
+                 | Add
+                 | Subtract
+                 | Multiply
+                 | IntDivide
+                 | ExecuteFunction Name
+                 | UntilNN Name Name [Command]
+                 | UntilNV Name Value [Command]
+                 | IfNN Name Name [Command] [Command]
+                 | IfNV Name Value [Command] [Command]
+        deriving (Show)
