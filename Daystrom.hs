1b9f4515892cd71f8d8ac8a0c25a4926ea37dc59
Jonah Rubin <jonah@jonaherubin.com>
2017-12-17 14:24:16 -0700

    parser started, some aliases and functions are parsable, but commands are placeholders

--- Daystrom.hs
+++ Daystrom.hs
@@ -3,18 +3,21 @@
 
 import qualified System.Environment
 import qualified System.FilePath
+import qualified Data.Char
 
 import qualified DaystromParser
 
 main :: IO ()
 main =
         do
-          args <- System.Enviroment.getArgs
+          args <- System.Environment.getArgs
           let fileName = case args of
                          [arg] -> arg
                          _ -> error "can only compile one file"
           -- Read the file (using lazy loading)
           file <- readFile fileName
-          let ast = DaystromParser.parseProgram file
+          let lowercaseFile = [Data.Char.toLower loweredString | loweredString <- file]
+          putStrLn ("'" ++ lowercaseFile ++ "'")
+          let ast = DaystromParser.parseProgram lowercaseFile
 
           putStrLn (show ast)
--- DaystromParser.hs
+++ DaystromParser.hs
@@ -11,14 +11,56 @@ import Control.Applicative hiding (optional)
 import DaystromSyntax
 import qualified Data.Char
 
+deadspace :: Parser ()
+deadspace = skipMany ((string " ") <|> (string "\n") <|> (string "\r\n"))
+
+punct :: Parser ()
+punct = skipMany ((sym ',') <|> (sym '.'))
+
+endline :: Parser ()
+endline = punct <-+-> deadspace
+
+program :: Parser Program
+program = ((many alias) <+-> deadspace) <+> (many function) >>=: \(al, fn) -> DaystromProgram al fn
+
+alias :: Parser Alias
+alias = ((aliasName <+-> ((text "is on") <|> (text "is now")) <+> stationName <+-> endline) >>=: \(al, st) -> StationAlias al st) <??> "malformed alias declaration"
+
+aliasName :: Parser Name
+aliasName = ident
+
+stationName :: Parser Name
+stationName = ident 
+
+function :: Parser Function
+function = (((between mainDecl mainEnd ((some command) <???> "improper commands in main function")) >>=: \c -> MainFunction c)
+         <|>
+           ((between functionDecl functionEnd ((ident <+-> endline <+> (many command)) <???> "improper commands in function")) >>=: \(i, c) -> HelperFunction i c))
 
 
+functionDecl :: Parser ()
+functionDecl = (aliasName <-+-> (many (sym ',')) <-+-> ((text "prepare") <|> (text "ready")) <-+-> (text "maneuver")) <??> "malformed function declaration"
+
+functionEnd :: Parser ()
+functionEnd = (endline <-+-> (text "end maneuver") <-+-> endline) <??> "missing or malformed function ending"
+
+mainDecl :: Parser ()
+mainDecl = ((text "captain") <-+-> (many (text "'")) <-+-> (sym 's') <-+-> (text "log") <-+-> garbage <-+-> deadspace) <??> "malformed main function start"
+
+mainEnd :: Parser ()
+mainEnd = (endline <-+-> (text "end log") <-+-> endline) <??> "missing or malformed main function ending"
+
+garbage :: Parser ()
+garbage = skipMany (alphanum <|> (sym ',') <|> (sym '.') <|> (sym ' '))
+
+command :: Parser Command
+command = (text "hi") <+-> endline >>=: \i -> Com i Swap
 
 ---------------------------
 -- MAIN PARSING FUNCTION --
 ---------------------------
 
-parseProgram :: String -> [Statement]
+parseProgram :: String -> Program
 parseProgram input =
-  parse programParser input
+  parse program input
 
